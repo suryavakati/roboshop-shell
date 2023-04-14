d357f22997b625dd067f785281d9af368e77f162
surya vakati <suryatejanaidu187@gmail.com>
2023-04-13 23:54:56 -0500

    modified mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,18 @@
 source common.sh
 
 print_head "creating mongo repo"
-cp ${code_dir}/config/mongo.repo /etc/yum.repos.d/mongo.repo &>>{log_file}
+cp ${code_dir}/config/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "installing mongodb"
-yum install mongodb-org -y &>>{log_file}
+yum install mongodb-org -y &>>${log_file}
 
 print_head "updating mongodb listener"
 sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
 
 print_head "enabling mongodb"
-systemctl enable mongod &>>{log_file}
+systemctl enable mongod &>>${log_file}
 
 print_head "starting mongodb"
-systemctl start mongod &>>{log_file}
+systemctl start mongod &>>${log_file}
 
 
