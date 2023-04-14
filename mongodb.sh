a8fc128e6c03d2860d572715d3303c1d34e0510d
surya vakati <suryatejanaidu187@gmail.com>
2023-04-13 22:47:16 -0500

    modified mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,13 @@
+source common.sh
+
+print_head "creating mongo repo"
 cp config/mongo.repo /etc/yum.repos.d/mongo.repo
+
+print_head "installing mongodb"
 yum install mongodb-org -y 
+
+print_head "enabling mongodb"
 systemctl enable mongod 
+
+print_head "starting mongodb"
 systemctl start mongod 
