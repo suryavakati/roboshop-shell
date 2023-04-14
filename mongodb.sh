b6fa305c8f4e4409619897e60d4797ffe05fb44b
surya vakati <suryatejanaidu187@gmail.com>
2023-04-13 23:57:13 -0500

    modified mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ print_head "installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 
 print_head "updating mongodb listener"
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 print_head "enabling mongodb"
 systemctl enable mongod &>>${log_file}
