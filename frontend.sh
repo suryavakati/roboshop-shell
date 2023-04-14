1ebba79084412ce877a24c82fd8801e4365f4549
surya vakati <suryatejanaidu187@gmail.com>
2023-04-13 22:39:53 -0500

    modified fronend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,29 @@
 code_dir=$(pwd)
-echo -e "\e[35m installing nginx \e[0m"
-yum install nginx -y 
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
 
-echo -e "\e[35m removing nginx html files \e[0m"
-rm -rf /usr/share/nginx/html/* 
+print_head{
+    echo -e "\e[35m $1 \e[0m"
+}
 
-echo -e "\e[35m extracting data from external source \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+print_head "installing nginx"
+yum install nginx -y &>>${log_file}
+
+print_head "removing nginx html content"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+
+print_head "Extracting content from external source"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+print_head "Unzipping frontend file"
+unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35m copying roboshop configuration from config placing in required location \e[0m"
-pwd
-cp ${code_dir}/config/roboshop.conf /etc/nginx/default.d/roboshop.conf 
+print_head "Copying content from config to required location"
+cp ${code_dir}/config/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-echo -e "\e[35m enabling nginx \e[0m"
-systemctl enable nginx 
+print_head "Enabling nginx"
+systemctl enable nginx &>>${log_file}
 
-echo -e "\e[35m starting nginx \e[0m"
-systemctl start nginx 
+print_head "Starting nginx"
+systemctl start nginx &>>${log_file}
