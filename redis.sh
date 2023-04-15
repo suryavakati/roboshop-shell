8c5d03f52f079ddea1b88c0e2ba9ee8d21c18c34
surya vakati <suryatejanaidu187@gmail.com>
2023-04-14 21:21:37 -0500

    modified redis

--- redis.sh
+++ redis.sh
@@ -12,6 +12,10 @@ print_head "installing redis package"
 yum install redis -y  &>>${log_file} 
 status_check $?
 
+print_head "updating listen adrress to 0.0.0.0"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}  
+status_check $?
+
 print_head "enabling and starting redis service"
 systemctl enable redis &>>${log_file} 
 systemctl start redis &>>${log_file} 
