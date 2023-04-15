2c7f1423dfa6026879453a6c931178d5248c0d40
surya vakati <suryatejanaidu187@gmail.com>
2023-04-14 21:03:18 -0500

    modified frontend  catalogue mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,23 @@
 source common.sh
 
 print_head "creating mongo repo"
-cp ${code_dir}/config/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+cp ${code_dir}/config/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}   
+status_check $?
 
 print_head "installing mongodb"
-yum install mongodb-org -y &>>${log_file}
+yum install mongodb-org -y &>>${log_file}   
+status_check $?
 
 print_head "updating mongodb listener"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}   
+status_check $?
 
 print_head "enabling mongodb"
 systemctl enable mongod &>>${log_file}
+status_check $?
 
 print_head "starting mongodb"
-systemctl start mongod &>>${log_file}
+systemctl start mongod &>>${log_file}  
+status_check $?
 
 
