a15cde321fae4525286c33a892c651e7d00ce071
surya vakati <suryatejanaidu187@gmail.com>
2023-04-15 20:52:12 -0500

    modified mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,7 @@ source common.sh
 mysql_root_password=$1
 if [ -z ${mysql_root_password} ]; then
     echo -e "\e[31m missing mysql root password \e[0m"
+    exit 1
 fi
 
 print_head "disbaling mysql"
